b21f040b12e37cf6aea8b6d40245605c010ba684
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-09 00:01:27 +0300

    Fix flake8 in views.py & settings.py

--- task/views.py
+++ task/views.py
@@ -31,7 +31,12 @@ class ToggleTaskCompletion(generic.UpdateView):
     model = Task
     fields = "__all__"
 
-    def post(self, request: HttpRequest, *args: str, **kwargs: reverse_lazy) -> HttpResponse:
+    def post(
+            self,
+            request: HttpRequest,
+            *args: str,
+            **kwargs: reverse_lazy
+    ) -> HttpResponse:
         task_id = request.POST.get("task_id")
         task = Task.objects.get(id=task_id)
 
--- todo_list/settings.py
+++ todo_list/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ct40n0f+g*k*oa+r&ekwwgo6*ubz&dt9j7fhck9@064%*8lot-"
+SECRET_KEY = "ct40n0f+g*k*oa+r&ekwwgo6*ubz&dt9j7fhck9@064%*8lot-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,16 +89,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
     },
 ]
 
