17e1eac67976ec880216d7f1f1cfc5aa9e734092
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-08 19:14:25 +0300

    Fix a typo in a path

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("task.urls.py", namespace="task")),
+    path("", include("task.urls", namespace="task")),
 ]
